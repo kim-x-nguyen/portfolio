220014c465e6d23577dfb3ece1d43245e9372819
Kim Nguyen <43408755+nguyenxuongkim2103@users.noreply.github.com>
2021-12-18 06:24:41 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,7 @@ const Home = () => {
       />
       <meta name="og:title" property="og:title" content="Kim Nguyen's web portfolio"/>
       <meta property="og:type" content="website" />
-      <meta name="og:description" property="og:description" content="Hi! My name is Kim Nguyen and welcome to my portfolio." />
+      <meta name="og:description" property="og:description" content="Hi, I am Kim, a web developer based in Finland. I have been working with React.js, Node.js, and MongoDB for more than two years. I am now looking for a career opportunity. Please, feel free contact me if you have any question related to web development." />
       <meta property="og:url" content="https://www.kimnguyen.tech/" />
       <meta property="og:image" content="https://www.kimnguyen.tech/images/kimnguyen.jpg"/>
       <meta property="og:image:width" content="1200" />
