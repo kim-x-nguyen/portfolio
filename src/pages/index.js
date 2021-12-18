d6b2ae8866660ee09a77efb0657cd66229383c7a
Kim Nguyen <43408755+nguyenxuongkim2103@users.noreply.github.com>
2021-12-18 06:13:21 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,9 +16,9 @@ const Home = () => {
     <Head>
       <title>Kim Nguyen - Portfolio</title>
       <link rel="shortcut icon" href="/images/favicon.ico" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
-      <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
+      <link rel="apple-touch-icon" sizes="90x90" href="/images/icons8-portfolio-90.png" />
+      <link rel="icon" type="image/png" sizes="60x60" href="/images/icons8-portfolio-60.png"/>
+      <link rel="icon" type="image/png" sizes="30x30" href="/images/icons8-portfolio-30.png"/>
       <link rel="icon" type="image/png" href="https://www.kimnguyen.tech/" />
       <meta
         name='description'
